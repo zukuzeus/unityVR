bbda5265def5673aadb8dd26525e67d11c2f9b82
omlet94 <n.omieczynski@gmail.com>
2017-12-02 19:29:02 +0100

    refactoring kodu zgodnie z review

--- Assets/Scripts/crumblesGenerator.cs
+++ Assets/Scripts/crumblesGenerator.cs
@@ -4,41 +4,36 @@ public class crumblesGenerator : MonoBehaviour {
 
     public GameObject Crumble;
     public GameObject Stone;
-    public static int numberOfCrumbles = 100;
-    public static int numberOfStones = (int)(numberOfCrumbles / 10);
     private GameObject Ground;
     private Vector3[] CrumblesCoordinates = new Vector3[numberOfCrumbles];
     private Vector3[] StonesCoordinates = new Vector3[numberOfStones];
-    private float x_min;
-    private float x_max;
-    private float z_min;
-    private float z_max;
-    private float f_x;
-    private float f_z;
-    private float margin=5;
+    public static int numberOfCrumbles = 100;
+    public static int numberOfStones = (int)(numberOfCrumbles / 10);
+    private float[] AreaBounds = new float[4];
+    private float fountain_x_half_size;
+    private float fountain_z_half_size;
+    private float marginFromFountain=5;
+    private float marginFromCrumble = 5;
     // Use this for initialization
     void Start () {
-        //Debug.Log("x_max " + x_max + " x_min " + x_min);
-        setArea();
-        FountainSizes();
-        PlaceObjects();
+        Debug.Log(Crumble.GetComponent<Renderer>().bounds.size.x);
+        Debug.Log(Crumble.GetComponent<Renderer>().bounds.size.z);
+        PlaceObjectsOnArea();
     }
-    void PlaceObjects()
+    void PlaceObjectsOnArea()
     {
+        SetAreaForPlacingObjects();
+        CalculateFountainSize();
         int all_size = numberOfCrumbles + numberOfStones;
         int j = 0;
-        bool new_value;
         Vector3[] AllCoordinates = new Vector3[all_size];
-
         
         for (int i = 0; i < all_size; i++)
         {
-            AllCoordinates[i] = GeneratedPosition();
-            new_value = CheckValues(AllCoordinates, i);
-            while ((AllCoordinates[i].x > -f_x - margin && AllCoordinates[i].x < f_x + margin && AllCoordinates[i].z > -f_z - margin && AllCoordinates[i].z < f_z + margin) || !new_value)
+            AllCoordinates[i] = GetPositionForObject();
+            while (CheckIfCoordinatesAreInsideFountain(AllCoordinates,i) || !CheckIfObjectCoordinatesAreNew(AllCoordinates, i))
             {
-                AllCoordinates[i] = GeneratedPosition();
-                new_value = CheckValues(AllCoordinates, i);
+                AllCoordinates[i] = GetPositionForObject();
             }
             if (i < numberOfCrumbles)
             {
@@ -52,8 +47,10 @@ public class crumblesGenerator : MonoBehaviour {
             }
            
         }
+    }bool CheckIfCoordinatesAreInsideFountain(Vector3[] AllCoordinates, int i) {
+        return (AllCoordinates[i].x > -fountain_x_half_size - marginFromFountain && AllCoordinates[i].x < fountain_x_half_size + marginFromFountain && AllCoordinates[i].z > -fountain_z_half_size - marginFromFountain && AllCoordinates[i].z < fountain_z_half_size + marginFromFountain);
     }
-    bool CheckValues(Vector3[] Coord, int i) {
+    bool CheckIfObjectCoordinatesAreNew(Vector3[] Coord, int i) {
         for (int j = 0; j < i; j++)
         {
             if (Coord[j]== Coord[i])
@@ -63,31 +60,29 @@ public class crumblesGenerator : MonoBehaviour {
         }
         return true;
     }
-    void FountainSizes()
+    void CalculateFountainSize()
     {
-        float fx_size = GameObject.Find("Fountain").GetComponent<Renderer>().bounds.size.x;
-        float fz_size = GameObject.Find("Fountain").GetComponent<Renderer>().bounds.size.z;
-        f_x = fx_size / 2;
-        f_z = fz_size / 2;
+        float fountain_x_size = GameObject.Find("Fountain").GetComponent<Renderer>().bounds.size.x;
+        float funtain_z_size = GameObject.Find("Fountain").GetComponent<Renderer>().bounds.size.z;
+        fountain_x_half_size = fountain_x_size / 2;
+        fountain_z_half_size = funtain_z_size / 2;
     }
-    void setArea()
+    void SetAreaForPlacingObjects()
     {       
         Ground = GameObject.Find("Pavement");
-        float x_size = Ground.GetComponent<Renderer>().bounds.size.x;
-        float z_size = Ground.GetComponent<Renderer>().bounds.size.z;
-        x_max = x_size/2 - 0.1f * x_size;
-        x_min = - x_max;
-        z_max = z_size/2 - 0.1f * z_size;
-        z_min = -z_max;
-        //Debug.Log("x_max " + x_max + " x_min " + x_min);
-        //Debug.Log("z_max " + z_max + " z_min " + z_min);
+        float area_x_size = Ground.GetComponent<Renderer>().bounds.size.x;
+        float area_z_size = Ground.GetComponent<Renderer>().bounds.size.z;
+        AreaBounds[0] = area_x_size/2 - 0.1f * area_x_size; // x_max
+        AreaBounds[1] = -AreaBounds[0];//x_min
+        AreaBounds[2] = area_z_size/2 - 0.1f * area_z_size;//z_max
+        AreaBounds[3] = -AreaBounds[2];//z_min
     }
-    Vector3 GeneratedPosition()
+    Vector3 GetPositionForObject()
     {
         float x,y,z;  
-        x = Random.Range(x_min, x_max);
+        x = Random.Range(AreaBounds[1], AreaBounds[0]);
         y = Crumble.transform.position.y; //get y from Crumble prefabs
-        z = Random.Range(z_min, z_max);
+        z = Random.Range(AreaBounds[3], AreaBounds[2]);
         return new Vector3(x, y, z);
     }
 }
