62bf6c2c455127063280d87472941f902e7f8275
zukuzeus <danielzukowskii@gmail.com>
2017-12-19 23:55:50 +0100

    wpierdalanie obiektów

---
+++ Assets/Scripts/EatObject.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatObject : MonoBehaviour
+{
+    public Material ObjectNormalMaterial;
+    public Material triggerMaterial;
+    public bool isPickable;
+    public string kindOfObject;
+    private bool playerInArea;
+    private bool isPickedUp;
+    private string tag;
+
+
+    void start()
+    {
+        triggerMaterial = GetComponent<Renderer>().material;
+        this.tag = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInArea && isPickable)
+        {
+            if (isPickedUp)
+                placeDown();
+            else
+                pickUp();
+        }
+    }
+    private void pickUp()
+    {
+        if (Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.JoystickButton1))
+        {
+            isPickedUp = true;
+            counterIncrement(kindOfObject, tag);
+
+            Destroy(gameObject);
+
+        }
+    }
+    private void placeDown()
+    {
+        if (Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.JoystickButton1))
+        {
+            isPickedUp = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "Bot")
+        {
+          
+            this.tag = other.tag;
+            Debug.Log("tag= " + other.tag);
+            GetComponent<Renderer>().material = triggerMaterial;
+            playerInArea = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "Bot")
+        {
+            this.tag = "";
+            playerInArea = false;
+            GetComponent<Renderer>().material = ObjectNormalMaterial;
+        }
+    }
+    private void counterIncrement(string pickableKind, string playerOrBot)
+    {
+        if (playerOrBot == "Player")
+        {
+            switch (pickableKind)
+            {
+                case "crumble":
+                    ObjectConsumables.CrumblesCounterPlayer++;
+                    Debug.Log("crumbles player= " + ObjectConsumables.CrumblesCounterPlayer);
+                    break;
+                case "stone":
+                    ObjectConsumables.StoneCounterPlayer++;
+                    Debug.Log("stones player= " + ObjectConsumables.StoneCounterPlayer);
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (playerOrBot == "Bot")
+        {
+            switch (pickableKind)
+            {
+                case "crumble":
+                    ObjectConsumables.CrumblesCounterBot++;
+                    Debug.Log("crumbles bot= " + ObjectConsumables.CrumblesCounterBot);
+                    break;
+                case "stone":
+                    ObjectConsumables.StoneCounterBot++;
+                    Debug.Log("stones bot= " + ObjectConsumables.StoneCounterBot);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- Assets/Scripts/crumblesGenerator.cs
+++ Assets/Scripts/crumblesGenerator.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
 
-public class crumblesGenerator : MonoBehaviour {
+public class crumblesGenerator : MonoBehaviour
+{
 
     public GameObject Crumble;
     public GameObject Stone;
     private GameObject Ground;
     private Vector3[] CrumblesCoordinates = new Vector3[numberOfCrumbles];
     private Vector3[] StonesCoordinates = new Vector3[numberOfStones];
-    public static int numberOfCrumbles = 100;
-    public static int numberOfStones = (int)(numberOfCrumbles / 10);
+    private static int numberOfCrumbles = 100;
+    private static int numberOfStones = (int)(numberOfCrumbles / 10);
     private float[] AreaBounds = new float[4];
     private float fountain_x_half_size;
     private float fountain_z_half_size;
@@ -17,7 +18,8 @@ public class crumblesGenerator : MonoBehaviour {
     private float marginFromFountain = 2.0f;
     private float marginFromCrumble = 0.5f;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         Debug.Log(Crumble.GetComponent<Renderer>().bounds.size.x);
         Debug.Log(Crumble.GetComponent<Renderer>().bounds.size.z);
         PlaceObjectsOnArea();
@@ -30,11 +32,11 @@ public class crumblesGenerator : MonoBehaviour {
         int all_size = numberOfCrumbles + numberOfStones;
         int j = 0;
         Vector3[] AllCoordinates = new Vector3[all_size];
-        
+
         for (int i = 0; i < all_size; i++)
         {
             AllCoordinates[i] = GetPositionForObject();
-            while (CheckIfCoordinatesAreInsideFountain(AllCoordinates,i) || !CheckIfObjectCoordinatesAreNew(AllCoordinates, i))
+            while (CheckIfCoordinatesAreInsideFountain(AllCoordinates, i) || !CheckIfObjectCoordinatesAreNew(AllCoordinates, i))
             {
                 AllCoordinates[i] = GetPositionForObject();
             }
@@ -43,22 +45,25 @@ public class crumblesGenerator : MonoBehaviour {
                 CrumblesCoordinates[i] = AllCoordinates[i];
                 Instantiate(Crumble, CrumblesCoordinates[i], Quaternion.identity);
             }
-            else {
+            else
+            {
                 StonesCoordinates[j] = AllCoordinates[i];
                 Instantiate(Stone, StonesCoordinates[j], Quaternion.identity);
                 j++;
             }
-           
+
         }
     }
-    bool CheckIfCoordinatesAreInsideFountain(Vector3[] AllCoordinates, int i) {
+    bool CheckIfCoordinatesAreInsideFountain(Vector3[] AllCoordinates, int i)
+    {
         return (AllCoordinates[i].x > -fountain_x_half_size - marginFromFountain && AllCoordinates[i].x < fountain_x_half_size + marginFromFountain && AllCoordinates[i].z > -fountain_z_half_size - marginFromFountain && AllCoordinates[i].z < fountain_z_half_size + marginFromFountain);
     }
-   
-    bool CheckIfObjectCoordinatesAreNew(Vector3[] Coordinates, int i) {
+
+    bool CheckIfObjectCoordinatesAreNew(Vector3[] Coordinates, int i)
+    {
         for (int j = 0; j < i; j++)
         {
-            if (Coordinates[i].x > Coordinates[j].x - crumble_x_half_size - marginFromCrumble && Coordinates[i].x < Coordinates[j].x +crumble_x_half_size + marginFromCrumble && Coordinates[i].z > Coordinates[j].z - crumble_z_half_size - marginFromCrumble && Coordinates[i].z < Coordinates[j].z + crumble_z_half_size + marginFromCrumble)
+            if (Coordinates[i].x > Coordinates[j].x - crumble_x_half_size - marginFromCrumble && Coordinates[i].x < Coordinates[j].x + crumble_x_half_size + marginFromCrumble && Coordinates[i].z > Coordinates[j].z - crumble_z_half_size - marginFromCrumble && Coordinates[i].z < Coordinates[j].z + crumble_z_half_size + marginFromCrumble)
             {
                 return false;
             }
@@ -78,18 +83,18 @@ public class crumblesGenerator : MonoBehaviour {
         fountain_z_half_size = funtain_z_size / 2;
     }
     void SetAreaForPlacingObjects()
-    {       
+    {
         Ground = GameObject.Find("Pavement");
         float area_x_size = Ground.GetComponent<Renderer>().bounds.size.x;
         float area_z_size = Ground.GetComponent<Renderer>().bounds.size.z;
-        AreaBounds[0] = area_x_size/2 - 0.1f * area_x_size; // x_max
+        AreaBounds[0] = area_x_size / 2 - 0.1f * area_x_size; // x_max
         AreaBounds[1] = -AreaBounds[0];//x_min
-        AreaBounds[2] = area_z_size/2 - 0.1f * area_z_size;//z_max
+        AreaBounds[2] = area_z_size / 2 - 0.1f * area_z_size;//z_max
         AreaBounds[3] = -AreaBounds[2];//z_min
     }
     Vector3 GetPositionForObject()
     {
-        float x,y,z;  
+        float x, y, z;
         x = Random.Range(AreaBounds[1], AreaBounds[0]);
         y = Crumble.transform.position.y; //get y from Crumble prefabs
         z = Random.Range(AreaBounds[3], AreaBounds[2]);
