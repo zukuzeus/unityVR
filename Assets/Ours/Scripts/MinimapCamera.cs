7cfb89cba9dc350d2e476e1339009f03f2b806f5
omlet94 <n.omieczynski@gmail.com>
2017-12-29 15:57:33 +0100

    fixed minimap

---
+++ Assets/Ours/Scripts/MinimapCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour {
+
+    public GameObject player;
+    public float cameraHeight = 20.0f;
+
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+        Vector3 rotation = new Vector3(90, player.transform.eulerAngles.y, 0);
+        pos.y += cameraHeight;
+        transform.position = pos;
+        transform.eulerAngles = rotation;
+
+    }
+}
